3a9e574110648d7718214d47669ac907f9424fb1
Teja ps <tejaps15112001@gmail.com>
2023-09-07 22:40:06 +0530

    PROMISES, ASYNC AND AWAIT

---
+++ script.js
@@ -0,0 +1,116 @@
+//Q1
+const openButton1 = document.getElementById("openButton1");
+  const closeButton1 = document.getElementById("closeButton1");
+  const imageContainer1 = document.getElementById("imageContainer1");
+  openButton1.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (212).png";
+    imageContainer1.appendChild(img);
+  });
+  closeButton1.addEventListener("click", () => {
+    imageContainer1.innerHTML = "";
+  });
+
+//Q2
+  const openButton2 = document.getElementById("openButton2");
+  const closeButton2 = document.getElementById("closeButton2");
+  const imageContainer2 = document.getElementById("imageContainer2");
+  openButton2.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (215).png";
+    imageContainer2.appendChild(img);
+  });
+  closeButton2.addEventListener("click", () => {
+    imageContainer2.innerHTML = "";
+  });
+
+  //3
+  const openButton3 = document.getElementById("openButton3");
+  const closeButton3 = document.getElementById("closeButton3");
+  const imageContainer3 = document.getElementById("imageContainer3");
+  openButton3.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (217).png";
+    imageContainer3.appendChild(img);
+  });
+  closeButton3.addEventListener("click", () => {
+    imageContainer3.innerHTML = "";
+  });
+
+  //4
+  const openButton4 = document.getElementById("openButton4");
+  const closeButton4 = document.getElementById("closeButton4");
+  const imageContainer4 = document.getElementById("imageContainer4");
+  openButton4.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (220).png";
+    imageContainer4.appendChild(img);
+  });
+  closeButton4.addEventListener("click", () => {
+    imageContainer4.innerHTML = "";
+  });
+
+  //5
+  const openButton5 = document.getElementById("openButton5");
+  const closeButton5 = document.getElementById("closeButton5");
+  const imageContainer5 = document.getElementById("imageContainer5");
+  openButton5.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (224).png";
+    imageContainer5.appendChild(img);
+  });
+  closeButton5.addEventListener("click", () => {
+    imageContainer5.innerHTML = "";
+  });
+
+  //6
+  const openButton6 = document.getElementById("openButton6");
+  const closeButton6 = document.getElementById("closeButton6");
+  const imageContainer6 = document.getElementById("imageContainer6");
+  openButton6.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (215).png";
+    imageContainer6.appendChild(img);
+  });
+  closeButton6.addEventListener("click", () => {
+    imageContainer6.innerHTML = "";
+  });
+
+  //7
+  const openButton7 = document.getElementById("openButton7");
+  const closeButton7 = document.getElementById("closeButton7");
+  const imageContainer7 = document.getElementById("imageContainer7");
+  openButton7.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (226).png";
+    imageContainer7.appendChild(img);
+  });
+  closeButton7.addEventListener("click", () => {
+    imageContainer7.innerHTML = "";
+  });
+
+  //8
+  const openButton8 = document.getElementById("openButton8");
+  const closeButton8 = document.getElementById("closeButton8");
+  const imageContainer8= document.getElementById("imageContainer8");
+  openButton8.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (229).png";
+    imageContainer8.appendChild(img);
+  });
+  closeButton8.addEventListener("click", () => {
+    imageContainer8.innerHTML = "";
+  });
+
+  //9
+  const openButton9 = document.getElementById("openButton9");
+  const closeButton9 = document.getElementById("closeButton9");
+  const imageContainer9= document.getElementById("imageContainer9");
+  openButton9.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = "Screenshot (231).png";
+    imageContainer9.appendChild(img);
+  });
+  closeButton9.addEventListener("click", () => {
+    imageContainer9.innerHTML = "";
+  });
